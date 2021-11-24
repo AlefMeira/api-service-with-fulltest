1e312f80a3427aafbc3e888add826c6f0c266b64
alefmeira <alef.chaves01@gmail.com>
2021-11-24 08:27:58 -0300

    interface user repository

---
+++ src/main/java/com/metodo/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.metodo.api.repositories;
+
+import com.metodo.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
