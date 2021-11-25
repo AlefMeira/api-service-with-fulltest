b0b3e9df6217b6a23bccf554ecedff2ce00f649e
alefmeira <alef.chaves01@gmail.com>
2021-11-25 11:23:51 -0300

    endpoint findAll para user

--- src/main/java/com/metodo/api/services/UserService.java
+++ src/main/java/com/metodo/api/services/UserService.java
@@ -2,7 +2,11 @@ package com.metodo.api.services;
 
 import com.metodo.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
