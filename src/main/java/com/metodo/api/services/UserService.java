769b8faf579c746173ceecac5019fe369c4af09f
alefmeira <alef.chaves01@gmail.com>
2021-11-25 12:02:27 -0300

    endpoint create para user

--- src/main/java/com/metodo/api/services/UserService.java
+++ src/main/java/com/metodo/api/services/UserService.java
@@ -1,12 +1,13 @@
 package com.metodo.api.services;
 
 import com.metodo.api.domain.User;
+import com.metodo.api.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
 
     User findById(Integer id);
-
     List<User> findAll();
+    User create(UserDTO obj);
 }
--- src/main/java/com/metodo/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/metodo/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.metodo.api.services.impl;
 
 import com.metodo.api.domain.User;
+import com.metodo.api.domain.dto.UserDTO;
 import com.metodo.api.repositories.UserRepository;
 import com.metodo.api.services.UserService;
 import com.metodo.api.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +18,24 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado, id: "+ id ));
     }
 
+    @Override
     public List<User> findAll(){
         return repository.findAll();
     }
+
+    @Override
+    public User create(UserDTO obj) {
+        obj.setId(null);
+       return repository.save(mapper.map(obj, User.class));
+    }
+
 }
