c09d036d3c8ca882054eb7fc7c2d3a79ea58ce85
alefmeira <alef.chaves01@gmail.com>
2021-11-26 11:33:11 -0300

    endpoint delete para user

--- src/main/java/com/metodo/api/resources/UserResource.java
+++ src/main/java/com/metodo/api/resources/UserResource.java
@@ -45,4 +45,10 @@ public class UserResource {
         obj.setId(id);
         return ResponseEntity.ok().body(mapper.map(service.update(obj),UserDTO.class));
     }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
